ae6eeb75ca98a388e83a555c8b6a71b3971b50f0
Carlos Barauna <cbarauna@MacBook-Air-de-Carlos.local>
2018-11-14 08:30:19 -0300

    Configuracões iniciais

---
+++ service/src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+
+const server = require('http').Server(app);
+
+server.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
