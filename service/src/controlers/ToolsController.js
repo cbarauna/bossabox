a3f734473464024966da37ac70beaa5670d247c7
Carlos Barauna <cbarauna@MacBook-Air-de-Carlos.local>
2018-11-17 14:14:15 -0300

    Implementação, Router e Contoller

--- service/src/controlers/ToolsController.js
+++ service/src/controlers/ToolsController.js
@@ -10,14 +10,15 @@ module.exports = {
         const tools = await Tools.create(req.body);
         res.json(tools);
     },
+    async show(req, res){
+        const tools = await Tools.findById(req.params.id);
+        return res.json(tools);
+    },
     async delete(req, res) {
         const tools = await Tools.findById(req.params.id);
         await tools.delete();
-        await tools await Tools.find({}).sort('-creat');
+       // const tools = await Tools.find({}).sort('-creat');
         return res.json(tools);
 
     }
-
-
-
 };
--- service/src/routes.js
+++ service/src/routes.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const routes = express.Router();
 
+const ToolsController = require('./controlers/ToolsController');
+
+
+routes.get('/tools', ToolsController.index);
+routes.post('/tools', ToolsController.store);
+routes.get('/tools/:id', ToolsController.show);
+routes.delete('/tools', ToolsController.delete);
+//routes.put('tools',ToolsController. );
 
 
 
